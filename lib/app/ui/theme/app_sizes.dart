4ad0dd827573d32595b5d1fa27f4fde948975c7e
mehdi031075 <mehdi031075@yahoo.com>
2022-12-06 22:52:09 +0330

    Add appSizes

---
+++ lib/app/ui/theme/app_sizes.dart
@@ -0,0 +1,7 @@
+class AppSizes {
+  static double smallPadding = 8;
+  static double mediumPadding = 12;
+  static double defaultPadding = 16;
+  static double largePadding = 32;
+  static double defaultRadius = 12;
+}
