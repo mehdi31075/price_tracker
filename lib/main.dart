69111359109f8583fe58e6a0c2e6672d9fec1bae
mehdi031075 <mehdi031075@yahoo.com>
2022-12-07 01:23:16 +0330

    Provide BLoC and Repository before start home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:price_tracker/app/cubit/active_symbols_cubit.dart';
+import 'package:price_tracker/app/data/providers/api_provider.dart';
+import 'package:price_tracker/app/data/repositories/api_repository.dart';
 import 'package:price_tracker/app/ui/pages/home_page.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: RepositoryProvider(
+        create: (_) => ApiRepository(apiProvider: ApiProvider()),
+        child: BlocProvider(
+          create: (context) => ActiveSymbolsCubit(
+            apiRepository: context.read<ApiRepository>(),
+          ),
+          child: Builder(
+            builder: (context) {
+              context.watch<ActiveSymbolsCubit>().getActiveSymbolRequest();
+              return const HomePage();
+            },
+          ),
+        ),
+      ),
     ),
   );
 }
