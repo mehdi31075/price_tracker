1f3e5b7f2ef108f2d8a3864654840d002f46188a
mehdi031075 <mehdi031075@yahoo.com>
2022-12-07 01:38:22 +0330

    Add markets filter in cubit class

--- lib/app/cubit/active_symbols_cubit.dart
+++ lib/app/cubit/active_symbols_cubit.dart
@@ -4,32 +4,47 @@ import 'package:price_tracker/app/data/repositories/api_repository.dart';
 
 class ActiveSymbolsCubit extends Cubit<GetActiveSymbolsState> {
   final ApiRepository apiRepository;
-  ActiveSymbolsCubit({required this.apiRepository}) : super( GetActiveSymbolsInitialState());
+  ActiveSymbolsCubit({required this.apiRepository}) : super(GetActiveSymbolsInitialState());
 
   getActiveSymbolRequest() async {
-    emit( GetActiveSymbolsLoadingState());
+    emit(GetActiveSymbolsLoadingState());
     try {
+      final markets = <String>[];
       apiRepository.getActiveSymbols().listen((activeSymbols) {
-        emit( GetActiveSymbolsSuccessState(activeSymbols: activeSymbols));
+        for (var symbol in activeSymbols) {
+          if (!markets.contains(symbol.market)) {
+            markets.add(symbol.market ?? '');
+          }
+        }
+        emit(
+          GetActiveSymbolsSuccessState(
+            activeSymbols: activeSymbols,
+            markets: markets,
+          ),
+        );
       });
     } on Exception catch (e) {
-      emit( GetActiveSymbolsErrorState(e: e));
+      emit(GetActiveSymbolsErrorState(e: e));
     }
   }
 }
 
 abstract class GetActiveSymbolsState {}
 
-class  GetActiveSymbolsInitialState extends GetActiveSymbolsState {}
+class GetActiveSymbolsInitialState extends GetActiveSymbolsState {}
 
-class  GetActiveSymbolsLoadingState extends GetActiveSymbolsState {}
+class GetActiveSymbolsLoadingState extends GetActiveSymbolsState {}
 
-class  GetActiveSymbolsSuccessState extends GetActiveSymbolsState {
-   GetActiveSymbolsSuccessState({required this.activeSymbols}) : super();
+class GetActiveSymbolsSuccessState extends GetActiveSymbolsState {
+  GetActiveSymbolsSuccessState({
+    required this.activeSymbols,
+    required this.markets,
+  }) : super();
   final List<ActiveSymbol> activeSymbols;
+  final List<String> markets;
 }
 
-class  GetActiveSymbolsErrorState extends GetActiveSymbolsState {
-   GetActiveSymbolsErrorState({required this.e}) : super();
+class GetActiveSymbolsErrorState extends GetActiveSymbolsState {
+  GetActiveSymbolsErrorState({required this.e}) : super();
   final Exception e;
 }
