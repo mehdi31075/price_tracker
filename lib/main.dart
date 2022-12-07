69dfab6697098bdab6eb73bdba1c240d824cd6e7
mehdi031075 <mehdi031075@yahoo.com>
2022-12-07 06:53:21 +0330

    Prove symBoleTickCubit bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:price_tracker/app/cubit/active_symbols_cubit.dart';
+import 'package:price_tracker/app/cubit/symbol_ticks_cubit.dart';
 import 'package:price_tracker/app/data/providers/api_provider.dart';
 import 'package:price_tracker/app/ui/pages/home_page.dart';
 
@@ -17,12 +18,24 @@ class _MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ActiveSymbolsCubit(
-        apiProvider: ApiProvider(),
-      ),
+    final apiProvider = ApiProvider();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ActiveSymbolsCubit>(
+          create: (context) => ActiveSymbolsCubit(
+            apiProvider: apiProvider,
+          ),
+        ),
+        BlocProvider<SymbolTicksCubit>(
+          create: (context) => SymbolTicksCubit(
+            apiProvider: apiProvider,
+          ),
+        ),
+      ],
       child: Builder(
         builder: (context) {
+          context.read<ActiveSymbolsCubit>().getActiveSymbolRequest();
+
           return const MaterialApp(
             debugShowCheckedModeBanner: false,
             home: HomePage(),
