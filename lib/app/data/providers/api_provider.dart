3453d9c8a9f913f9b7d26a821025589158ca98a7
mehdi031075 <mehdi031075@yahoo.com>
2022-12-07 06:51:53 +0330

    Add getSymbolTicks API

--- lib/app/data/providers/api_provider.dart
+++ lib/app/data/providers/api_provider.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 
 import 'package:price_tracker/app/data/endpoints.dart';
 import 'package:price_tracker/app/data/models/requests/get_active_symbols_request.dart';
+import 'package:price_tracker/app/data/models/requests/get_symbol_ticks_request.dart';
 import 'package:price_tracker/app/data/models/responses/get_active_symbols_response.dart';
+import 'package:price_tracker/app/data/models/responses/get_symbol_ticks_response.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ApiProvider {
@@ -21,6 +23,22 @@ class ApiProvider {
       await for (var element in channel.stream) {
         yield GetActiveSymbolsResponse.fromJson(jsonDecode(element));
         channel.sink.close();
+      }
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  Stream<GetSymbolTicksResponse> getSymbolTicks({required GetSymbolTicksRequest request}) async* {
+    try {
+      final channel = WebSocketChannel.connect(
+        Uri.parse('${Endpoints.baseUrl}?app_id=$appId'),
+      );
+      channel.sink.add(
+        jsonEncode(request.toJson()),
+      );
+      await for (var element in channel.stream) {
+        yield GetSymbolTicksResponse.fromJson(jsonDecode(element));
       }
     } on Exception {
       rethrow;
